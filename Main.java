2ad26bb007d7dd46949a8e396acd21bb24bc15b4
HaoXK <haoxuank@nycstudents.net>
2024-10-01 12:35:58 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,12 +2,10 @@ public class Main
 {
     public static void main(String[] args)
     {
-    String info;
-    Scoreboard game = new Scoreboard("Red", "Blue");
-    info = game.getScore();
-    System.out.println(info);
-    game.recordPlay(1);
-    info = game.getScore();
-    System.out.println(info);
+        String info;
+        Scoreboard game = new Scoreboard("Red", "Blue");
+        game.recordPlay(3);
+        info = game.getScore();
+        System.out.println(info);
     }
 }
