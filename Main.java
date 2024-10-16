782860426a82bda56cdd42d6c0bd296be20ab39a
HaoXK <haoxuank@nycstudents.net>
2024-10-16 12:23:26 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,6 +8,16 @@ public class Main
         game.recordPlay(0);
         game.recordPlay(3);
         info = game.getScore();
-        System.out.println(info);
+        System.out.println(game.getScore());
+        game.recordPlay(1);
+        game.recordPlay(0);
+        System.out.println(game.getScore());
+        game.recordPlay(0);
+        game.recordPlay(4);
+        game.recordPlay(0);
+        System.out.println(game.getScore());
+        Scoreboard match = new Scoreboard("Lions", "Tigers");
+        System.out.println(match.getScore());
+        System.out.println(game.getScore());
     }
 }
